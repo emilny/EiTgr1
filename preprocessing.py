98c2f4f5e37a8d45635fabdea97bd0f3b73ef0aa
Fredrik Almås <fredralm@stud.ntnu.no>
2021-02-17 12:56:53 +0100

    add tqdm and savedata

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@ import cv2  # Computer vision library, image handling
 import matplotlib.pyplot as plt  # For checking images along the way
 import random  # For shuffling data
 import os
+from tqdm import tqdm
 
 TRAINING_DATA_DIR = "Data/train/"
 TEST_DATA_DIR = "Data/test/"
@@ -16,6 +17,12 @@ cats_to_onehots = {"COVID19": [1, 0, 0],
 
 IMG_SIZE = 100
 
+CLASS_WEIGHTS = {0: 33.0,
+                 1: 34.0,
+                 2: 33.0}
+
+
+
 def create_dataset(percentage_of_data_set, training=True):
     data_dir = TRAINING_DATA_DIR if training else TEST_DATA_DIR
     data_x = []
@@ -23,7 +30,7 @@ def create_dataset(percentage_of_data_set, training=True):
     for cat in CATEGORIES:
         onehot = np.array(cats_to_onehots[cat])
         path = os.path.join(data_dir, cat)
-        for i, img in enumerate(os.listdir(path)):
+        for i, img in enumerate(tqdm(os.listdir(path))):
             if i > percentage_of_data_set*len(os.listdir(path)):
                 break
             img_array = cv2.imread(os.path.join(path, img))
@@ -35,9 +42,14 @@ def create_dataset(percentage_of_data_set, training=True):
     np.random.shuffle(index_list)
     x_data = [data_x[i] for i in index_list]
     y_data = [data_y[i] for i in index_list]
+    if training:
+        np.save('training_data', x_data)
+        np.save('training_labels', y_data)
+    else:
+        np.save('test_data', x_data)
+        np.save('test_labels', y_data)
     return x_data, y_data
 
-create_dataset(percentage_of_data_set=0.01, training=True)  # Generates 1% of the training data set
 
 
-
+train_data, train_labels = create_dataset(percentage_of_data_set=0.1, training=True)  # Generates 1% of the training data set
