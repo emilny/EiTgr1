53ec5fc95e5414453430a890b95b0a5c0ac076f0
emilny <emil.nylen.forthun@gmail.com>
2021-02-17 11:07:04 +0100

    Begun preprocessing file

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,12 @@
 import numpy as np  # Linalg library
 import pandas as pd  # Data processing, easy to handle dataframes
 import cv2  # Computer vision library, image handling
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # For checking images along the way
+import random  # For shuffling data
 
-training_data_dir = "Data/train"
-test_data_dir = "Data/test"
+TRAINING_DATA_DIR = "Data/train"
+TEST_DATA_DIR = "Data/test"
 
-
-
+CATEGORIES = ["COVID19", "NORMAL", "PNEUMONIA"]
 
 
