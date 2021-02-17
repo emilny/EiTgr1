881d2518baa8d55aa62b28ffc47a8d1e6c36f62d
emilny <emil.nylen.forthun@gmail.com>
2021-02-17 12:29:18 +0100

    Generalized data extraction code

--- preprocessing.py
+++ preprocessing.py
@@ -16,21 +16,27 @@ cats_to_onehots = {"COVID19": [1, 0, 0],
 
 IMG_SIZE = 100
 
-def create_train_data():
-    train_x = []
-    train_y = []
+def create_dataset(data_dir, percentage_of_data_set):
+    data_x = []
+    data_y = []
     for cat in CATEGORIES:
         onehot = np.array(cats_to_onehots[cat])
-        path = os.path.join(TRAINING_DATA_DIR, cat)
-        for img in os.listdir(path):
+        path = os.path.join(data_dir, cat)
+        for i, img in enumerate(os.listdir(path)):
+            if i > percentage_of_data_set*len(os.listdir(path)):
+                break
             img_array = cv2.imread(os.path.join(path, img))
             resized_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-            train_x.append(np.array(resized_array))
-            train_y.append(onehot)
+            data_x.append(np.array(resized_array))
+            data_y.append(onehot)
 
-    index_list = [i for i in range(len(train_x))]
+    index_list = [i for i in range(len(data_x))]
     np.random.shuffle(index_list)
-    x_train = [train_x[i] for i in index_list]
-    y_train = [train_y[i] for i in index_list]
+    x_data = [data_x[i] for i in index_list]
+    y_data = [data_y[i] for i in index_list]
+    return x_data, y_data
+
+create_dataset(TRAINING_DATA_DIR, 0.01)  # Generates 1% of the training data set
+
 
 
