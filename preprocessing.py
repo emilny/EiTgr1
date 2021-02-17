30b299c9dc6dbf0615d119c329b8698c330dc638
emilny <emil.nylen.forthun@gmail.com>
2021-02-17 10:52:45 +0100

    Begun preprocessing file

---
+++ preprocessing.py
@@ -0,0 +1,12 @@
+import numpy as np  # Linalg library
+import pandas as pd  # Data processing, easy to handle dataframes
+import cv2  # Computer vision library, image handling
+import matplotlib.pyplot as plt
+
+training_data_dir = "Data/train"
+test_data_dir = "Data/test"
+
+
+
+
+
