669ad951601c262231eb7925bb3dadce04521f18
emilny <emil.nylen.forthun@gmail.com>
2021-02-24 11:08:33 +0100

    minor changes

--- test_cnn.py
+++ test_cnn.py
@@ -5,12 +5,12 @@ from tflearn import DNN
 import preprocessing
 import os
 
-preprocessing.create_dataset(1)
+#preprocessing.create_dataset(1)
 
 LR = 0.01
 MODEL_NAME = 'covid_test-{}-{}.model'.format(LR, '2conv-basic')
 
-convnet = input_data(shape=[None, preprocessing.IMG_SIZE, preprocessing.IMG_SIZE, 1], name='input')
+convnet = input_data(shape=[64, preprocessing.IMG_SIZE, preprocessing.IMG_SIZE, 3], name='input')
 
 convnet = conv_2d(convnet, 32, 5, activation='relu')
 convnet = max_pool_2d(convnet, 5)
@@ -31,6 +31,11 @@ if os.path.exists('{}.meta'.format(MODEL_NAME)):
     print('model loaded!')
 
 X, Y = preprocessing.load_dataset(train=True)
-model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_batch_size=0.1,
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+model.fit(X_inputs=X,
+          Y_targets=Y,
+          n_epoch=3,
+          validation_batch_size=0.1,
+          snapshot_step=500,
+          show_metric=True,
+          run_id=MODEL_NAME)
 
