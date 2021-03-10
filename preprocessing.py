ca4c9917c5c54600a855e0924d6457b03e64ec91
eliassr <eliasravnestad@gmail.com>
2021-03-10 12:57:58 +0100

    Updated data augmentation

--- preprocessing.py
+++ preprocessing.py
@@ -48,6 +48,7 @@ def create_dataset(percentage_of_data_set=1., training=True, augmented_versions=
             data_x.append(np.array(resized_array))
             data_y.append(onehot)
     data_x = np.array(data_x)
+
     # This is where training data is increased in size by way of data augmentation:
     if training:
         data_x = augment_data_set(orig_x=data_x, batch_size=augmented_versions)
@@ -55,7 +56,6 @@ def create_dataset(percentage_of_data_set=1., training=True, augmented_versions=
     index_list = [i for i in range(len(data_x))]
     np.random.shuffle(index_list)
     x_data = [data_x[i] for i in index_list]
-
 
     norm_x_data = keras.utils.normalize(x_data)
     y_data = [data_y[i] for i in index_list]
@@ -68,29 +68,27 @@ def create_dataset(percentage_of_data_set=1., training=True, augmented_versions=
         np.save('test_labels', y_data)
 
 
-def augment_data_set(orig_x, batch_size, rotate_range=15, shear_range=0.1, vertical_flip=True, zoom_range=0.15):
+def augment_data_set(orig_x, batch_size):
     """
     Augments images by performing several (=batch_size) augmenting transformations
     Saves augmented images to augmented folder
     :param orig_x: Input images as np.arrays
-    :param batch_size: number of "new", augmented images to generate per original img
-    :param rotate_range: maximum angle of rotation
-    :param shear_range: maximam degree of shear applied
-    :param vertical_flip: whether or not to allow mirroring along vertical axis
-    :param zoom_range: Max zoom (most likely applied in the middle of img)
     :return:
     """
-    datagen = ImageDataGenerator(rotation_range=rotate_range,
-                                 shear_range=shear_range,
-                                 vertical_flip=vertical_flip,
-                                 zoom_range=zoom_range)
+    datagen = ImageDataGenerator(rescale=1./255,
+                                 zoom_range=0.3,
+                                 rotation_range=15,
+                                 width_shift_range=0.1,
+                                 height_shift_range=0.1,
+                                 shear_range=0.1,
+                                 horizontal_flip=True,
+                                 fill_mode='nearest')
     datagen.fit(orig_x)
     for batch in datagen.flow(x=orig_x, batch_size=batch_size):
         im = batch[0]
         plt.imshow(im)
         plt.show()
         exit()
-
     return None
 
 
@@ -111,8 +109,11 @@ def load_dataset(train=True):
     return x_data, y_data
 
 
-if __name__ == '__main__':
-    create_dataset(0.01)
+#if __name__ == '__main__':
+#    create_dataset(0.01)
     # x = load_dataset(train=True)[0][0]
     # plt.imshow(x)
     # plt.show()
+
+
+create_dataset(percentage_of_data_set=1., training=True, augmented_versions=3)
